2e9258981fd2f596b38ff037a2b6117f4bc5ff5b
Menard Ysrael Cruz <ysrael214@gmail.com>
2019-08-27 09:39:17 +0800

    initial commit

---
+++ upd_csreps/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for upd_csreps project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'upd_csreps.settings')
+
+application = get_wsgi_application()
