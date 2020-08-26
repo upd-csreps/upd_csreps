0be9979779d526d18da45215d48fc36c8c415758
Menard Ysrael Cruz <ysrael214@gmail.com>
2020-08-27 02:05:04 +0800

    ccc logo change + troubleshooter response template

--- troubleshooter/views.py
+++ troubleshooter/views.py
@@ -2,11 +2,12 @@ from datetime import datetime
 from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 import secrets
 
-def sheet_query(page_ct=1, ql_command='select *'):
+def sheet_query(page_ct=1, ql_command='select *',qtype=1):
 	results = None
 	page_ct = page_ct-1
 
@@ -23,17 +24,16 @@ def sheet_query(page_ct=1, ql_command='select *'):
 				range="Query!A1:L", body={})
 
 			body = {
-				'values': [["=QUERY(Data!A2:L, \"{0}\")".format(ql_command)]]
+				'values': [["=QUERY(Data!A2:L, \"{0} order by A desc limit 10 offset {1}\")".format(ql_command, page_ct*10)]]
 			}
+
+			to_write = 'Query{0}'.format(qtype if qtype!= 1 else '')
 			
 			service.spreadsheets().values().update(
-			spreadsheetId=settings.TSHOOT_SPREADSHEET, range='Query!A1:A1',
+			spreadsheetId=settings.TSHOOT_SPREADSHEET, range='{0}!A1:A1'.format(to_write),
 			valueInputOption='USER_ENTERED', body=body).execute()
 
-			range_names = ['Query!A{0}:L{1}'.format( 
-				str(1+(page_ct*10)), 
-				str(10+(page_ct*10))
-			)]
+			range_names = ['{0}!A1:L'.format(to_write)]
 			result = service.spreadsheets().values().batchGet(
 				spreadsheetId=settings.TSHOOT_SPREADSHEET, ranges=range_names).execute()
 			results = result.get('valueRanges', [])	
@@ -51,14 +51,25 @@ def admin(request):
 		context = {}
 		return render(request, 'troubleshooter/admin.html', context)
 	elif request.method == 'POST':
+		action = request.GET.get('act', None)
+		ql_command = None
+		qtype = 1
 
-		if request.GET.get('act', None) == "concerns":
-			concerns = sheet_query(ql_command="select * where L = ''")
+		if action == "concerns":
+			ql_command = "select * where L = ''"
+		elif action == "answers":
+			ql_command = "select * where L != ''"
+			qtype = 2
 
+		if ql_command:
+			concerns = sheet_query(ql_command=ql_command, qtype=qtype)
 			if type(concerns) != 'int':
 				new_concerns = []
 				for concern in concerns:
-					new_concerns.append({'date': concern[0],
+					if concern[0] == '#N/A':
+						break
+
+					to_app = {'date':  datetime.strptime(concern[0], '%Y-%m-%d %H:%M:%S'),
 						'alias': concern[1],
 						'type': concern[2],
 						'details': concern[3],
@@ -68,13 +79,23 @@ def admin(request):
 						'message': concern[7],
 						'consultation': (concern[8] == 'TRUE'),
 						'hidden': (concern[9] == 'TRUE'),
-						'token' : concern[10]
-					})
-				# context['concerns'] = new_concerns
+						'token' : concern[10],
+						'answer': ''
+					}
 
-				return JsonResponse({'response': new_concerns})
+					try:
+						to_app['answer'] = concern[11]
+					except:
+						pass
+
+					new_concerns.append(to_app)
+
+				response = render_to_string('troubleshooter/partials/admin_response.html', {'concerns': new_concerns}).strip()
+
+				return JsonResponse({'response': response})
 
 		return None
+
 		
 def index(request):
 	concern_type = [
@@ -99,7 +120,7 @@ def index(request):
 			if type(concerns) != 'int':
 				new_concerns = []
 				for concern in concerns:
-					new_concerns.append({'date': concern[0],
+					new_concerns.append({'date': datetime.strptime(concern[0], '%Y-%m-%d %H:%M:%S'),
 						'alias': concern[1],
 						'type': concern[2],
 						'details': concern[3],
