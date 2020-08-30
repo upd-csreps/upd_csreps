f2526f2dba5737bcbee055687ce9193e816671ea
Menard Ysrael Cruz <ysrael214@gmail.com>
2020-08-30 17:22:09 +0800

    fixed indents

--- main/management/commands/twitter_retweet.py
+++ main/management/commands/twitter_retweet.py
@@ -1,5 +1,3 @@
-
-
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 import tweepy
@@ -41,7 +39,6 @@ class Command(BaseCommand):
 				status = api.get_status(status.id, tweet_mode="extended")
 				retweeted = False
 				lacks_time = False
-				retweet_is_own = False
 				is_reply = bool(status.in_reply_to_status_id)
 
 				 #If tweet date is > 10 days from now
@@ -49,14 +46,14 @@ class Command(BaseCommand):
 
 				try:
 					# If the source retweet is already retweeted.
-				    if (status.retweeted_status.retweeted):
-				    	retweeted = True
-				    # If not yet retweeted, ignore tweet if retweet source is self or banned accounts.
-				    elif (status.retweeted_status.user == api.me()) or (status.retweeted_status.user.screen_name in banned_twitter_handles):
-				    	continue
+					if (status.retweeted_status.retweeted):
+						retweeted = True
+					# If not yet retweeted, ignore tweet if retweet source is self or banned accounts.
+					elif (status.retweeted_status.user == api.me()) or (status.retweeted_status.user.screen_name in banned_twitter_handles):
+						continue
 				except AttributeError:  # Not a Retweet from Page
-				    if (status.retweeted):
-				    	retweeted = True
+					if (status.retweeted):
+						retweeted = True
 
 				# Checking if time between tweets are at least 90 minutes.
 				# This is to avoid live tweets being retweeted.
@@ -65,7 +62,7 @@ class Command(BaseCommand):
 					if elapsed_minutes < 90:
 						lacks_time = True 
 
-				if not (retweeted or is_reply or lacks_time or too_old or retweet_is_own):
+				if not (retweeted or is_reply or lacks_time or too_old):
 					status.retweet()
 
 				previous_tweet_time = status.created_at
